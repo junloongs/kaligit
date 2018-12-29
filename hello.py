5e500bd02a16925e88a3815ad12f30b3d22c9517
root <root@localhost.localdomain>
2018-12-29 11:35:58 +0800

    函数的参数（必选参数，默认参数，可变参数，命名关键字参数，关键字参数）

--- hello.py
+++ hello.py
@@ -184,7 +184,90 @@ def my_abs(x1):
 	else:
 		return -x1
 print(my_abs(-929))#-->992
+#函数返回多个值：
+import math
+def move(x,y,step,angle=0):#坐标、位移和角度，角度是默认参数，不填写自动置0. 特别注意：“定义默认参数要牢记一点：默认参数必须指向不变对象！” 
+	radian = math.pi/180*angle#30度对应的[弧度]的定义是，当角所对的弧长等于半径时，角的大小为1弧度。角所对的弧长是半径的几倍，那么角的大小就是几弧度：角（弧度）＝弧长/半径 ；180度＝π弧度 ；
+										#1度＝π/180 弧度；弧度＝度×π/180，反过来，π弧度＝180°；1弧度＝180°/π （≈57.3°）；度＝弧度×180°/π 。
+	nx = x+step*math.cos(radian)#余弦×斜边=邻边;计算参考：https://blog.csdn.net/lyd1212/article/details/47359611
+	ny = y+step*math.sin(radian)#正弦×斜边=对边
+	return nx,ny
+x ,y =move(100,100,60,30)
+print(x,y)#-->151.96152422706632 130.0
+#原来返回值是一个tuple！但是，在语法上，返回一个tuple可以省略括号，而多个变量可以同时接收一个tuple，按位置赋给对应的值，所以，Python的函数返回多值其实就是返回一个tuple，但写起来更方便。
+print(math.sqrt(2))#-->1.4142135623730951#平方根计算。
+#定义可变参数函数：要定义出这个函数，我们必须确定输入的参数。由于参数个数不确定，我们首先想到可以把a，b，c……作为一个list或tuple传进来，这样，函数可以定义如下：
+def calc(numbers):
+    sum = 0
+    for n in numbers:
+        sum = sum + n * n
+    return sum
+print(calc([1,2,5,6]))#-->66
+def calc2(*numbers):#加*号以后，在函数内部，参数numbers接收到的是一个tuple。
+    sum = 0
+    for n in numbers:
+        sum = sum + n * n
+    return sum
+num1=[1,2,5,6]
+print(calc2(*num1))#-->66#Python允许你在list或tuple前面加一个*号，把list或tuple的元素变成可变参数传进去。
+#关键字参数:可变参数允许你传入0个或任意个参数，这些可变参数在函数调用时自动组装为一个tuple。而关键字参数允许你传入0个或任意个含参数名的参数，这些关键字参数在函数内部自动组装为一个dict.
+def person(name,age,**kw):
+	print('name:',name,'age:',age,'other:',kw)
 
+person('Michael',30)#-->name: Michael age: 30 other: {}
+person('Bob',35,gender='M',job='PythonEngineer',city='Beijing')#-->name: Bob age: 35 other: {'gender': 'M', 'job': 'PythonEngineer', 'city': 'Beijing'}
+extra_dict={'city': 'Beijing', 'job': 'Engineer'}
+person('Jack', 24, **extra_dict)#-->name: Jack age: 24 other: {'city': 'Beijing', 'job': 'Engineer'}
+#**extra表示把extra这个dict的所有key-value用关键字参数传入到函数的**kw参数，kw将获得一个dict，注意kw获得的dict是extra的一份拷贝，对kw的改动不会影响到函数外的extra。
+#命名关键字参数：对于关键字参数，函数的调用者可以传入任意不受限制的关键字参数。至于到底传入了哪些，就需要在函数内部通过kw检查。
+def person(name, age, **kw):
+    if 'city' in kw:
+        # 有city参数
+        pass
+    if 'job' in kw:
+        # 有job参数
+        pass
+    print('name:', name, 'age:', age, 'other:', kw)
+#但是调用者仍可以传入不受限制的关键字参数。如果要限制关键字参数的名字，就可以用命名关键字参数，例如，只接收city和job作为关键字参数。这种方式定义的函数如下：
+def person(name, age, *, city, job):#和关键字参数**kw不同，命名关键字参数需要一个特殊分隔符*，*后面的参数被视为命名关键字参数。
+    print(name, age, city, job)
+
+person('Jack', 24, city='Beijing', job='Engineer')#-->Jack 24 Beijing Engineer
+
+def person(name, age, *args, city, job):#如果函数定义中已经有了一个可变参数，后面跟着的命名关键字参数就不再需要一个特殊分隔符*了.
+    print(name, age, args, city, job)
+#注意：命名关键字参数必须传入参数名，这和位置参数不同。如果没有传入参数名，调用将报错：
+#person('Jack', 24, 'Beijing', 'Engineer')#-->TypeError: person() takes 2 positional arguments but 4 were given,由于调用时缺少参数名city和job，
+#Python解释器把这4个参数均视为位置参数，但person()函数仅接受2个位置参数。
+def person(name, age, *, city='Beijing', job):#命名关键字参数可以有缺省值，从而简化调用,
+    print(name, age, city, job)
+#由于命名关键字参数city具有默认值，调用时，可不传入city参数：
+person('Jack', 24, job='Engineer')#-->Jack 24 Beijing Engineer
+#注意：使用命名关键字参数时，要特别注意，如果没有可变参数，就必须加一个*作为特殊分隔符。如果缺少*，Python解释器将无法识别位置参数和命名关键字参数：
+def person(name, age, city, job):
+    # 缺少 *，city和job被视为位置参数
+    pass
+#参数组合：在Python中定义函数，参数定义的顺序必须是：必选参数、默认参数、可变参数、命名关键字参数和关键字参数，这5种参数都可以组合使用。
+def f1(a, b, c=0, *args, **kw):
+    print('a =', a, 'b =', b, 'c =', c, 'args =', args, 'kw =', kw)
+
+def f2(a, b, c=0, *, d, **kw):
+    print('a =', a, 'b =', b, 'c =', c, 'd =', d, 'kw =', kw)
+
+f1(1, 2)#-->a = 1 b = 2 c = 0 args = () kw = {}
+f1(1, 2, c=3)#-->a = 1 b = 2 c = 3 args = () kw = {}
+f1(1, 2, 3, 'a', 'b')#-->a = 1 b = 2 c = 3 args = ('a', 'b') kw = {}
+f1(1, 2, 3, 'a', 'b', x=99)#-->a = 1 b = 2 c = 3 args = ('a', 'b') kw = {'x': 99}
+f2(1, 2, d=99, ext=None)#-->a = 1 b = 2 c = 0 d = 99 kw = {'ext': None}
+#最神奇的是通过一个tuple和dict，你也可以调用上述函数：
+args = (1, 2, 3, 4)
+kw = {'d': 99, 'x': '#'}
+f1(*args, **kw)#-->a = 1 b = 2 c = 3 args = (4,) kw = {'d': 99, 'x': '#'}
+args = (1, 2, 3)
+kw = {'d': 88, 'x': '#'}
+f2(*args, **kw)#-->a = 1 b = 2 c = 3 d = 88 kw = {'x': '#'}
+#所以，对于任意函数，都可以通过类似func(*args, **kw)的形式调用它，无论它的参数是如何定义的。
+#!! 虽然可以组合多达5种参数，但不要同时使用太多的组合，否则函数接口的可理解性很差。
 
 
 
