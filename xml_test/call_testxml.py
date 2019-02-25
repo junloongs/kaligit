a574f5db54b5e4afd209539244cc03bb7c4f628f
junloongs <928750380@qq.com>
2019-02-25 09:27:05 +0800

    read xml files

---
+++ xml_test/call_testxml.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog, QFileDialog
+from testxml import Ui_MainWindow
+import os,csv
+from xml.dom import minidom
+import time
+class MainForm(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super(MainForm, self).__init__()
+        self.setupUi(self)
+        # 菜单的点击事件，当点击关闭菜单时连接槽函数 close()
+        #self.fileCloseAction.triggered.connect(self.close)
+        # 菜单的点击事件，当点击打开菜单时连接槽函数 openMsg()
+        #self.fileOpenAction.triggered.connect(self.openMsg)
+        self.pushButton.clicked.connect(self.openMsg)
+        self.pushButton_2.clicked.connect(self.getxml)
+    def openMsg(self):
+        global directory1
+        m = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+        s = '2019-12-31 00:00:00'
+        if m < s:
+            directory1 = QFileDialog.getExistingDirectory(self,
+                                                          "选取文件夹",
+                                                          "./")  # 起始路径
+            # 在状态栏显示文件地址
+            self.statusbar.showMessage(directory1)
+        else:
+            self.statusbar.showMessage("软件已过期，请与作者联系！", 10000)
+    def getxml(self):
+        try:
+            work_dir = directory1
+            w_dir = ''.join(list(directory1)[0:2])
+            with open(w_dir+'/xml_result.csv', 'a', newline='') as f1:
+                writer = csv.writer(f1)
+                writer.writerow(("MR任务名称", "EnodeB名称", "EnodeBID", "最大UE数"))
+            for parent, dirnames, filenames in os.walk(work_dir, followlinks=True):
+                for filename in filenames:
+                    filetype = os.path.splitext(filename)
+                    filename1, type = filetype
+                    if type =='.xml':
+                        file_path = os.path.join(parent, filename)
+                        # 使用minidom解析器打开 XML 文档
+                        with open(file_path, 'r', encoding='GBK') as fh:
+                            dom = minidom.parse(fh)
+                            # 加载dom对象元素
+                            root = dom.documentElement  # root存储根节点元素的相关属性
+                            enodeBs = root.getElementsByTagName('enodeB')
+                            for enb in enodeBs:
+                                MR_name = root.getAttribute('name')
+                                enbname = enb.getAttribute('managedElementType')
+                                ueMaxNum = root.getElementsByTagName('ueMaxNum')[0]
+                                enbids = enb.getElementsByTagName('EnodeBID')[0]
+                                ueMaxNum_data = ueMaxNum.childNodes[0]
+                                enbid = enbids.getAttribute('eNodeBId')
+                                #print(MR_name, enbname, enbid, ueMaxNum_data.data)
+                                with open(w_dir+'/xml_result.csv', 'a', newline='') as f:
+                                    writer = csv.writer(f)
+                                    writer.writerow((MR_name, enbname, enbid, ueMaxNum_data.data))
+                        fh.close()
+                        # 在状态栏显示文件地址
+            self.statusbar.showMessage("结果保存在："+w_dir+"/xml_result.csv")
+        except Exception as err:
+            self.statusbar.showMessage("请选择xml文件所在文件夹")
+            return
+
+app = QApplication(sys.argv)
+win = MainForm()
+win.show()
+sys.exit(app.exec_())
---
+++ xml_test/testxml.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'testxml.ui'
+#
+# Created by: PyQt5 UI code generator 5.11.3
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QFileDialog
+import sys
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWin")
+        MainWindow.resize(320, 200)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(50, 30, 141, 20))
+        self.label.setObjectName("label")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(188, 30, 75, 23))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(100, 80, 75, 23))
+        self.pushButton_2.setObjectName("pushButton_2")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 23))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "中兴MR任务xml解析"))
+        self.label.setText(_translate("MainWindow", "选取xml文件所在文件夹："))
+        self.pushButton.setText(_translate("MainWindow", "选取文件夹"))
+        self.pushButton_2.setText(_translate("MainWindow", "执行"))
+
